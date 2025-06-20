eb35575f98c73fb73cd23d1fdcd5fb1f5be22195
Jose Rodolfo Gonzalez <jrrodolfo3@gmail.com>
2025-06-19 18:15:53 -0600

    agregando pwa para navegar

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,18 @@ import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
+import { environment } from './environments/environment';
+
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()), provideServiceWorker('ngsw-worker.js', {
+           enabled: environment.production,
+             //enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
 
